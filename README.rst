ae197472359cc461d5bd6c7c765150a211b59b34
Nikola Pavlov <nikola.pavlov1955@gmail.com>
2021-08-06 12:14:44 +0300

    0.5 major rework

---
+++ README.rst
@@ -0,0 +1,17 @@
+TODO:
+-----
+
+* better search for team  (Elastic Search)
+* caching  api requests
+* pagination in the index.html
+* deploy to Vagran Target with Ansible
+
+* add tests
+* improve code quality
+
+
+How to run
+==========
+
+
+pmr
--- website/settings.py
+++ website/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# openligadb settings
+API_URL = 'https://www.openligadb.de/api'
+LEAGUE = 'bl1'
+SEASON = '2021'
